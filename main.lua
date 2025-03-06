6aa218069396a1c0a5d3a154dc908f5d4e79d178
RobertoPalici <palici.roberto@gmail.com>
2025-03-06 23:05:07 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -31,7 +31,7 @@ SMODS.Joker{
         return {vars = {center.ability.extra + 1}}
     end,
     calculate = function(self,card,context)
-        if context.repetition and
+        if context.repetition and context.cardarea == G.play and
           (context.other_card:get_id() == 7 or
            context.other_card:get_id() == 8 or
            context.other_card:get_id() == 9) then
@@ -51,7 +51,7 @@ SMODS.Joker{
             '{X:mult,C:white}X#1#{} Mult',
             'if the sum of values',
             'of scoring hand',
-            'is less or equal to 21'
+            'is less or equal to C:{attention}21{}'
         }
     },
     atlas = 'Jokers',
@@ -90,12 +90,47 @@ SMODS.Joker{
 SMODS.Joker{
     key = 'rob_massive_joker',
     loc_txt = {
-        name = 'Massive Joker'
-        text = 'This Joker gains',
-                    '{C:mult}+#2#{} Mult when each',
-                    'played {C:attention}Ace{} is scored',
-                    '{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)',
+        name = 'Massive Joker',
+        text = {
+            'This Joker gains',
+            '{C:mult}+#2#{} Mult when each',
+            'played {C:attention}Ace{} is scored',
+            '{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)'
+            }
     },
     atlas = 'Jokers',
-    pos = {x = 2, y = 0 }
-}
+    pos = {x = 2, y = 0 },
+    loc_vars = function(self, info_queue, center)
+        return {vars = { center.ability.extra.mult,
+                         center.ability.extra.mult_mod
+                        }
+                }
+    end,
+    config = {
+        extra = {
+            mult = 0,
+            mult_mod = 2
+        }
+    },
+    calculate = function(self, card, context)
+        if context.individual then
+            if context.cardarea == G.play then
+                if context.other_card:get_id() == 14 and not context.blueprint then
+                    card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_mod
+                    return{
+                        message = localize('k_upgrade_ex'),
+                        colour = G.C.RED
+                    }
+                end
+            end
+        elseif context.joker_main then
+            if card.ability.extra.mult > 0 then
+            return{
+                message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}},
+                mult_mod = card.ability.extra.mult,
+                colour = G.C.RED
+            }
+            end
+        end
+    end
+}   
