38e1d49570260064e6901977af50cd6c63d380b0
RobertoPalici <palici.roberto@gmail.com>
2025-03-10 18:04:16 +0200

    architect added

--- main.lua
+++ main.lua
@@ -5,6 +5,33 @@
 --- MOD_DESCRIPTION: Exemplu
 --- PREFIX: xmpl
 
+-- Store the original load function
+local original_set_ability = Card.set_ability
+local original_load = Card.load
+
+
+Card.load = function(self, cardTable, other_card)
+    original_load(self, cardTable, other_card)
+    
+    if self.config.center and self.config.center.name == "j_RobMod_rob_massive_joker" then
+        self.T.h = self.T.h * 1.25
+        self.T.w = self.T.w * 1.25
+    end
+end
+
+Card.set_ability = function(self, center, initial, delay_sprites)
+    --print("Setting ability for:", center and center.name or "Unknown")
+
+    original_set_ability(self, center, initial, delay_sprites)
+
+    if center and center.name == "j_RobMod_rob_massive_joker" then
+        print("Resizing Massive Joker in set_ability")
+        self.T.h = self.T.h * 1.35 
+        self.T.w = self.T.w * 1.35 
+        --print("New dimensions - Width:", self.T.w, "Height:", self.T.h)
+    end
+end
+
 SMODS.Atlas{
     key = 'Jokers',
     path = 'Jokers.png',
@@ -22,6 +49,8 @@ SMODS.Joker{
             '{C:attention}7{}, {C:attention}8{}, {C:attention}9{}'
         }
     },
+    rarity = 1,
+    cost = 4,
     atlas = 'Jokers',
     pos = {x = 0, y = 0 },
     config = {
@@ -51,9 +80,11 @@ SMODS.Joker{
             '{X:mult,C:white}X#1#{} Mult',
             'if the sum of values',
             'of scoring hand',
-            'is less or equal to C:{attention}21{}'
+            'is less or equal to {C:attention}21{}'
         }
     },
+    rarity = 2,
+    cost = 6,
     atlas = 'Jokers',
     pos = {x = 1, y = 0 },
     config = {
@@ -96,8 +127,10 @@ SMODS.Joker{
             '{C:mult}+#2#{} Mult when each',
             'played {C:attention}Ace{} is scored',
             '{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)'
-            }
+        }
     },
+    rarity = 3,
+    cost = 8,
     atlas = 'Jokers',
     pos = {x = 2, y = 0 },
     loc_vars = function(self, info_queue, center)
@@ -125,12 +158,72 @@ SMODS.Joker{
             end
         elseif context.joker_main then
             if card.ability.extra.mult > 0 then
+                return{
+                    message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}},
+                    mult_mod = card.ability.extra.mult,
+                    colour = G.C.RED
+                }
+            end
+        end
+    end
+}
+
+SMODS.Joker{
+    key = 'rob_architect_joker',
+    loc_txt = {
+        name = 'Architect',
+        text = {
+            'This Joker has {C:green}#1# in #2#{}',
+            'chance to create a', 
+            '{C:dark_edition}Negative{} {C:attention}Blueprint{}',
+            'when {C:attention}Blind{} is selected'
+        }
+    },
+    rarity = 3,
+    cost = 10,
+    blueprint_compat = false,
+    atlas = 'Jokers',
+    pos = {x = 3, y = 0 },
+    config = {
+        extra = 10
+    },
+    loc_vars = function(self, info_queue, center)
+        return {vars = {G.GAME.probabilities.normal, center.ability.extra}}
+    end,
+    calculate = function(self, card, context)
+        if context.setting_blind and not context.blueprint then
+            if pseudorandom('architect') < G.GAME.probabilities.normal/card.ability.extra then
+                local new_blueprint = create_card('Joker', G.jokers, nil,nil,nil,nil, 'j_blueprint')
+                new_blueprint:set_edition({negative = true}, true)
+                new_blueprint:add_to_deck()
+                G.jokers:emplace(new_blueprint)
             return{
-                message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}},
-                mult_mod = card.ability.extra.mult,
-                colour = G.C.RED
+                message = localize('k_plus_blueprint'),
+                colour = G.C.BLUE
             }
             end
         end
     end
-}   
+    
+}
+
+G.P_CENTERS['rob_massive_joker'] = {
+    name = 'Massive Joker',
+    text = {
+        'This Joker gains',
+        '{C:mult}+#2#{} Mult when each',
+        'played {C:attention}Ace{} is scored',
+        '{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)'
+    },
+    config = {
+        extra = {
+            mult = 0,
+            mult_mod = 2
+        }
+    },
+    pos = {x = 2, y = 0 },
+    atlas = 'Jokers'
+}
+
+-- Debug: Verify that the Massive Joker is registered
+print("Massive Joker registered:", G.P_CENTERS['rob_massive_joker'] and G.P_CENTERS['rob_massive_joker'].name or "Not found")
